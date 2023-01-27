5cb19a0e7850d06bc64a3750034f0a6f5f4a8619
jack1232 <jackjack5810@yahoo.com>
2023-01-27 13:17:41 -0500

    Update helper.ts

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -55,8 +55,4 @@ export const InitWebGPU = async (canvas: HTMLCanvasElement) => {
 
 export const CheckWebGPUSupport = navigator.gpu? 'Great, your current browser supports WebGPU!' : 
     `Your current browser does not support WebGPU! Make sure you are on a system 
-    with WebGPU enabled. Currently, WebGPU is only supported in  
-    <a href="https://www.google.com/chrome/canary/">Chrome canary</a>
-    with the flag "enable-unsafe-webgpu" enabled. See the 
-    <a href="https://github.com/gpuweb/gpuweb/wiki/Implementation-Status"> 
-    Implementation Status</a> page for more details.`;
+    with WebGPU enabled.`;
